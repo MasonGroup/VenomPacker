2defcf627b45d0edcb5085a2e51f9826f1dfb3cf
Freemasonry <95870255+MasonGroup@users.noreply.github.com>
2024-01-26 15:43:02 +0300

    Add files via upload

---
+++ VenomPacker.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Windows.Forms;
+
+class Program
+{
+    static void Main()
+    {
+        MessageBox.Show("Gangsta freemasonry made me do it", "VenomPacker");
+        Environment.Exit(0);
+    }
+}
